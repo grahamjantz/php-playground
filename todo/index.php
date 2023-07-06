3c26502ee3eedce9882336df40307deb1ac9d389
grahamjantz <grahammrjantz@gmail.com>
2023-07-06 11:55:35 -0600

    finished todo app

--- todo/index.php
+++ todo/index.php
@@ -12,42 +12,98 @@
     <link rel="stylesheet" href="css/styles.css" >
 </head>
 <body>
-    <div class="main-secion">
+    <div class="main-section">
         <div class="add-section">
-            <form action="">
-                <input type="text" name="title" id="" placeholder="This field is required"/>
+            <form action="app/add.php" method="POST" autocomplete="off">
+                <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error') {?>
+                    <input 
+                        type="text" 
+                        name="title"
+                        style="border-color: #ff6666"
+                        placeholder="This field is required"/>
+                    <button type="submit">Add &nbsp; <span>&#43;</button>
+                <?php } else {?>
+                <input 
+                    type="text" 
+                    name="title" 
+                    id="" 
+                    placeholder="What do you need to do?"/>
                 <button type="submit">Add &nbsp; <span>&#43;</button>
+                <?php }?>
             </form>
+
         </div>
         <?php
-            $todo = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+            $todos = $conn->query("SELECT * FROM todos ORDER BY id");
         ?>
         <div class="show-todo-section">
-            <?php if($todos->rowCount() > 0){ ?>
-                <div class="todo-item">
-                    <div class="empty">
-                        <img src="img/f.png" width="100%"/>
-                        <img src="img/Ellipsis.gif" width="80%"/>
-                    </div>
-                </div>
-            <?php }; ?>
             <?php if($todos->rowCount() <= 0){ ?>
                 <div class="todo-item">
                     <div class="empty">
-                        <img src="img/f.png" width="100%" />
-                        <img src="img/Ellipsis.gif" width="80px">
+                        <img src="img/f.png" width="100%"/>
+                        <img src="img/Ellipsis.gif" width="80px"/>
                     </div>
                 </div>
             <?php } ?>
-            <div class="to-do-item">
-                <input type="checkbox" name=" " id=""/>
-                <h2>This is hsafd sf</h2>
-                <br/>
-                <small>created: 07/06/2023</small>
-            </div>
-            
+
+            <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+                <div class="todo-item">
+                    <span id="<?php echo $todo['id']; ?>"
+                    class="remove-to-do">x</span>
+                    <?php if($todo['checked']) {?>
+                        <input type="checkbox" name=" " 
+                            data-todo-id ="<?php echo $todo['id']; ?>"    
+                            class="check-box"
+                            checked/>
+                        <h2 class="checked"><?php echo $todo['title'] ?></h2>
+                        <?php }else {?>
+                            <input type="checkbox" 
+                                data-todo-id ="<?php echo $todo['id']; ?>"    
+                                class="check-box"/>
+                        <h2><?php echo $todo['title'] ?></h2>
+                    <?php }?>
+                    <br>
+                    <small>Created: <?php echo $todo['date_time']?></small>
+                </div>
+            <?php } ?>
         </div>
 
     </div>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.remove-to-do').click(function() {
+                const id = $(this).attr('id');
+                $.post("app/remove.php",
+                    {
+                        id: id
+                    },
+                    (data) => {
+                        if(data) {
+                            $(this).parent().hide(600);
+                        }
+                    }
+                )
+            })
+            $('.check-box').click(function(e) {
+                const id = $(this).attr('data-todo-id')
+                $.post("app/check.php",
+                    {
+                        id: id
+                    },
+                    (data) => {
+                        if(data != 'error') {
+                            const h2 = $(this).next();
+                            if(data === '1') {
+                                h2.removeClass('checked')
+                            } else {
+                                h2.addClass('checked')
+                            }
+                        }
+                    }
+                )
+            })
+        })
+    </script>
 </body>
 </html>
